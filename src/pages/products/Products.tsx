a33aac777ed43b09cf2084b2758b0db65fd990ec
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-10 20:31:31 -0500

    feat(products): creation of table

---
+++ src/pages/products/Products.tsx
@@ -0,0 +1,10 @@
+import { ProductsTable } from "./components/ProductsTable";
+
+export const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <ProductsTable />
+    </div>
+  );
+};
