652ee83be9e72ed0d98b39253877a50b51595423
CesarCr300 <cc30122003@gmail.com>
2024-05-10 21:40:05 -0500

    refactor(products): moving logic to ProductsCardForm component

--- src/pages/productsCard/ProductsCard.tsx
+++ src/pages/productsCard/ProductsCard.tsx
@@ -1,36 +1,10 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getById } from "./application/productsCard.application";
-import useFetchAndLoad from "../../hooks/useFetchAndLoad";
-import { ProductModel } from "./model/product.model";
+import { Container } from "@mui/material";
+import { ProductsCardForm } from "./components/ProductsCardForm";
 
 export const ProductsCard = () => {
-  const { id } = useParams();
-  const [product, setProduct] = useState<ProductModel | null>(null);
-  const [loading, setLoading] = useState(false);
-  const { callEndpoint } = useFetchAndLoad(setLoading);
-
-  useEffect(() => {
-    try {
-      if (!id || isNaN(+id)) return;
-      getById(id as unknown as number, callEndpoint, setProduct);
-    } catch (error) {
-      //
-    }
-  }, []);
-
   return (
-    <div>
-      {!loading && !product && <h1>Producto no encontrado</h1>}
-      {product && (
-        <div>
-          <h1>{product.title}</h1>
-          <p>{product.price}</p>
-          <p>{product.stock}</p>
-          <p>{product.weightInGrams}</p>
-          <p>{product.comparePrice}</p>
-        </div>
-      )}
-    </div>
+    <Container>
+      <ProductsCardForm />
+    </Container>
   );
 };
