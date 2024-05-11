ecc4407d0f8aa98c93ddc9215a046ab5169ddac6
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-11 18:43:39 -0500

    styles: deleting innecesary margins

--- src/pages/productsCard/components/ProductsCardHeader.tsx
+++ src/pages/productsCard/components/ProductsCardHeader.tsx
@@ -2,7 +2,7 @@ import { GoBack } from "../../../components/GoBack";
 
 export const ProductsCardHeader = () => {
   return (
-    <div className="" style={{ marginBottom: "20px", marginTop: "20px" }}>
+    <div className="" style={{ marginBottom: "20px" }}>
       <GoBack to="/products" />
     </div>
   );
