c8624df9ae220883ae1e38883b195b2ab8ba4e98
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-11 17:31:22 -0500

    refactor(products): implementing context

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,10 +1,16 @@
+import { Container } from "@mui/material";
 import { ProductsTable } from "./components/ProductsTable";
+import { ProductsContext } from "./context/ProductsContext";
+import { useState } from "react";
 
 export const Products = () => {
+  const [loading, setLoading] = useState(false);
   return (
-    <div>
-      <h1>Products</h1>
-      <ProductsTable />
-    </div>
+    <ProductsContext.Provider value={{ loading, setLoading }}>
+      <Container>
+        <h1>Productos</h1>
+        <ProductsTable />
+      </Container>
+    </ProductsContext.Provider>
   );
 };
