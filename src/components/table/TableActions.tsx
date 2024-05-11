a48df47c160574d72acc7541a5491fbdadd58925
CesarCr300 <cc30122003@gmail.com>
2024-05-11 18:08:36 -0500

    fix(ProductTable): commenting properly

--- src/components/table/TableActions.tsx
+++ src/components/table/TableActions.tsx
@@ -39,9 +39,9 @@ export const TableActions = ({
           <EditIcon onClick={() => update(params.id as number, updateParams)} />
         </>
       )}
-      //The functionToDeleteFromTable is being used in this case because in this
+      {/*The functionToDeleteFromTable is being used in this case because in this
       case there are a lot of products, and its more effective to remove it this
-      way instead of filtering the rows
+      way instead of filtering the rows */}
       {remove && (
         <DeleteIcon
           onClick={() => {
