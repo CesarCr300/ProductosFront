42f75a1d7cb689366597aa87a913d441b40411c3
CesarCr300 <cc30122003@gmail.com>
2024-05-10 22:04:56 -0500

    refactor(productsCard): creation of useProductModelState to get the data froma product by Id

--- src/pages/productsCard/components/ProductsCardForm/ProductsCardForm.tsx
+++ src/pages/productsCard/components/ProductsCardForm/ProductsCardForm.tsx
@@ -2,18 +2,18 @@ import { useForm } from "react-hook-form";
 
 import { FormContainer } from "../../../../components/Form/FormContainer";
 import { ProductUpdate } from "../../model/product-update.model";
-import { ProductModel } from "../../model/product.model";
 import { useParams } from "react-router-dom";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import useFetchAndLoad from "../../../../hooks/useFetchAndLoad";
-import { getById, update } from "../../application/productsCard.application";
+import { update } from "../../application/productsCard.application";
 import { productsCardFormFields } from "./productsCardForm.fields";
+import { useProductModelState } from "../../hooks/useProductModelState";
 
 export const ProductsCardForm = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState<ProductModel | null>(null);
   const [loading, setLoading] = useState(false);
   const { callEndpoint } = useFetchAndLoad(setLoading);
+  const { product } = useProductModelState(id, callEndpoint);
   const { register, handleSubmit } = useForm<ProductUpdate>();
 
   const onSubmit = (data: any) => {
@@ -24,15 +24,6 @@ export const ProductsCardForm = () => {
     () => productsCardFormFields(register, product),
     [product]
   );
-
-  useEffect(() => {
-    try {
-      if (!id || isNaN(+id)) return;
-      getById(id as unknown as number, callEndpoint, setProduct);
-    } catch (error) {
-      //
-    }
-  }, []);
 
   if (product == null) return <></>;
 
