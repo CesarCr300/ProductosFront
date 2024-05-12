77cb75146d88bc2464e167bfce6457e82f9c4915
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-11 20:44:50 -0500

    styles(Table): adjusting witdth of columns

--- src/pages/products/products.columns.tsx
+++ src/pages/products/products.columns.tsx
@@ -1,11 +1,22 @@
+import { Typography } from "@mui/material";
 import { GridColDef } from "@mui/x-data-grid";
 
 export const getColumns = (): GridColDef[] => {
   return [
-    { field: "title", headerName: "Título", flex: 1 },
-    { field: "price", headerName: "Precio", flex: 1 },
-    { field: "stock", headerName: "Stock", flex: 1 },
-    { field: "weightInGrams", headerName: "Peso (g)", flex: 1 },
-    { field: "comparePrice", headerName: "Precio de comparación", flex: 1 },
+    {
+      field: "title",
+      headerName: "Título",
+      flex: 1,
+      minWidth: 100,
+    },
+    { field: "price", headerName: "Precio", flex: 1, minWidth: 90 },
+    { field: "stock", headerName: "Stock", flex: 0.75, minWidth: 60 },
+    { field: "weightInGrams", headerName: "Peso (g)", flex: 1, minWidth: 100 },
+    {
+      field: "comparePrice",
+      headerName: "Precio de comparación",
+      flex: 1,
+      minWidth: 100,
+    },
   ];
 };
