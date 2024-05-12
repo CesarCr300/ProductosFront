8b1c3e32ac3013cb8ad098b652ab0e65c2fcdd3f
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-11 22:58:29 -0500

    fix(SideBarLinks): removing innecesary duplicated keys

--- src/components/Sidebar/SidebarLinks.tsx
+++ src/components/Sidebar/SidebarLinks.tsx
@@ -5,12 +5,12 @@ export const SidebarLinks = () => {
   return (
     <div>
       <List>
-        <ListItemButton sx={{ padding: 0 }} key="Starred">
+        <ListItemButton sx={{ padding: 0 }}>
           <ListItemText>
             <CustomeLink to="/products" text="Productos" />
           </ListItemText>
         </ListItemButton>
-        <ListItemButton sx={{ padding: 0 }} key="Starred">
+        <ListItemButton sx={{ padding: 0 }}>
           <ListItemText>
             <CustomeLink to="/users/update" text="Actualizar usuario" />
           </ListItemText>
