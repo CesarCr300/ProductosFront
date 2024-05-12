16e4e455a372d101a7d3222cc2079d64807a8986
CesarCr300 <cc30122003@gmail.com>
2024-05-12 10:16:00 -0500

    feat(userRecoverPassword): creating form without functionality

--- src/pages/userRecoverPassword/components/UserRecoverPasswordForm.tsx
+++ src/pages/userRecoverPassword/components/UserRecoverPasswordForm.tsx
@@ -1,3 +1,25 @@
+import { useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
+
+import useFetchAndLoad from "../../../hooks/useFetchAndLoad";
+import { Button } from "../../../components/Button";
+import { FormContainer } from "../../../components/Form/FormContainer";
+import { UserRecoverPasswordModel } from "../models/user-recover-password.model";
+import { userRecoverPasswordFields } from "../userRecoverPassword.fields";
+
 export const UserRecoverPasswordForm = () => {
-  return <div>UserForgotPasswordForm</div>;
+  const [, setLoading] = useState(false);
+  const { callEndpoint } = useFetchAndLoad(setLoading);
+  const { handleSubmit, register } = useForm<UserRecoverPasswordModel>();
+
+  const onSubmit = (data: UserRecoverPasswordModel) => {};
+
+  return (
+    <FormContainer
+      fields={useMemo(() => userRecoverPasswordFields(register), [])}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Button text="Guardar" type="submit" />
+    </FormContainer>
+  );
 };
