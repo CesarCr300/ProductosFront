04edd1b00d5e80e58fa88d9770a985843e3eb6dd
CesarCr300 <cc30122003@gmail.com>
2024-05-13 12:34:20 -0500

    feat: implementing lazy loading in ProductsCard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import { Login } from "../pages/login/Login";
 import { Layout } from "../components/Layout";
 import { Products } from "../pages/products/Products";
-import { ProductsCard } from "../pages/productsCard/ProductsCard";
+const ProductsCard = lazy(() => import("../pages/productsCard/ProductsCard"));
 import { ProductsCreation } from "../pages/productsCreation/ProductsCreation";
 import { UserUpdate } from "../pages/userUpdate/UserUpdate";
 import { UserRecoverPassword } from "../pages/userRecoverPassword/UserRecoverPassword";
