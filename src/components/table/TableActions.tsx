723882ef9e06780b5fba4eb6751ef56bac43a15b
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-14 10:23:30 -0500

    fix(TableActions): removing new functionaly to remove the row by the dom because it was generating problems with React

--- src/components/table/TableActions.tsx
+++ src/components/table/TableActions.tsx
@@ -39,18 +39,10 @@ export const TableActions = ({
           <EditIcon onClick={() => update(params.id as number, updateParams)} />
         </>
       )}
-      {/*The functionToDeleteFromTable is being used in this case because in this
-      case there are a lot of products, and its more effective to remove it this
-      way instead of filtering the rows */}
       {remove && (
         <DeleteIcon
           onClick={() => {
-            remove(params.id as number, {
-              ...removeParams,
-              functionToDeleteFromTable: () => {
-                ref.current?.parentElement?.parentElement?.remove();
-              },
-            });
+            remove(params.id as number, removeParams);
           }}
         ></DeleteIcon>
       )}
